2b624704e47a0afd766aaf1301fce4d926d8ecbb
Noam Benkler <nbenkler@sift.net>
2024-04-11 15:18:52 -0400

    changed styles

--- swaparoo/index.php
+++ swaparoo/index.php
@@ -1,8 +1,9 @@
 <?php session_start(); include 'functions.php'; shared_header('Home') ?>
 
 <div class="featured">
-    <h2>Under Development</h2>
-    <p>~Under Development~</p>
+    <img src="../../images/logo.webp" alt="Swap-a-Roo Logo">
+    <h2>Swap-a-Roo</h2>
+    <p>Where Treasures Hop from Pocket to Pocket!</p>
 </div>
 </div>
 
