61ed423d01051ae9dbaf490c2a4cd8e2157ebdc4
Casey Owen <casey.michael.owen@gmail.com>
2024-04-16 13:34:43 -0400

    Added favicon to browser tab

--- swaparoo/functions.php
+++ swaparoo/functions.php
@@ -45,6 +45,7 @@ function user_header($title, $num_items_in_cart) {
     <head>
         <meta charset="utf-8">
         <title><?= $title ?></title>
+        <link rel="icon" href="/images/logo.webp">
         <link href="/swaparoo/styles/style.css" rel="stylesheet" type="text/css">
         <link href="/swaparoo/styles/registration.css" rel="stylesheet" type="text/css">
         <link href="/swaparoo/styles/account.css" rel="stylesheet" type="text/css">
